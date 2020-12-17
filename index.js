078dcb5aa6ca3e8a7682b998562843a73bf7166b
abhishekra07 <abhira719@gmail.com>
2020-12-17 17:12:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,43 +6,42 @@ const uuid = require('uuid')
 const app = express()
 const PORT = process.env.PORT || 3200
 
-const db = new JsonDB(new Config("JsonDB", true, false, '/'));
+//gettig json db instance
+const db = new JsonDB(new Config('JsonDB', true, false, '/'));
 
 //adding middleware
 app.use(express.json())
 
+//root url
 app.get('/', function (req, res) {
   res.status(200).json({
-    message: "Welcome to the application. You can use below paths!",
+    message: 'Welcome to the application. You can use below paths!',
     paths: "[ {'/user/add' to add user}]"
   })
 })
 
+
 app.post('/user/add', (req, res) => {
-  // console.log('body ',req.body);
   try {
     const name = req.body.username
     const mobile = req.body.mobile
     const hobby = req.body.hobby
     const id = uuid.v4()
     db.push(`/users/${id}`, { id, name, mobile, hobby }, true)
-    res.status(200).json({messgae:"user added!",id})
+    res.status(200).json({messgae:'user added!',id})
   } catch (e) {
-    console.log("Error :- ",e);
-    res.status(500).json({message: "Error in insert"})
+    console.log('Error :- ',e);
+    res.status(500).json({message: 'Error in insert'})
   }
 })
 
-
 app.get('/user/get', (req, res) => {
   const data = Object.values(db.getData('/users'));
+  let users = []
   data.map( value => {
-    console.log("value ",value);
+    users.push(value)
   })
-
-  // console.log("data :- ",data);
-
-  res.status(200).json({});
+  res.status(200).json({ users });
 })
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
