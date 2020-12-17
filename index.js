830f7be2e928e4218a7690480b5dcbdda2c4f68a
abhishekra07 <abhira719@gmail.com>
2020-12-17 11:59:58 +0530

    added JsonDB

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const { JsonDB } = require('node-json-db')
+const { Config } = require('node-json-db/dist/lib/JsonDBConfig')
+
 const app = express()
+const PORT = process.env.PORT || 3200
+
+const db = new JsonDB(new Config("JsonDB", true, false, '/'));
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000, () => console.log('server running on port 3000'))
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
